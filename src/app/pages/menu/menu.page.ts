1b73d945629014a1a6b24167b6f9d2126e07e2fb
JOSE LUIS MU ORIZAGA <jlmuo@yahoo.com>
2025-02-10 13:46:27 -0600

    Update 10 Feb 2025 a las 13:46

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,20 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from 'src/environments/environment';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {
+  }
+
+  onClickCapturar() {
+    console.log('capturar');
+    this.router.navigateByUrl(environment.paginaCapturar);
+    this.router.navigateByUrl(environment.paginaCapturar, { state: { data: undefined } });
+  }
+
+  onClickEditar() {
+    console.log('Editar');
+    this.router.navigateByUrl(environment.paginaEditar);
+  }
+
+  onClickExportar() {
+    console.log('Exportar');
+    this.router.navigateByUrl(environment.paginaExportar);
   }
 
 }
