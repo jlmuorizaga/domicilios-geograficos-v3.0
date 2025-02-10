9c5bb22f5fce53a363797c24639828baa3f44a47
JOSE LUIS MU ORIZAGA <jlmuo@yahoo.com>
2025-02-10 13:49:53 -0600

    Create capturar 10 Feb 2025 a las 13:49

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,4 +14,8 @@ export const routes: Routes = [
     path: 'menu',
     loadComponent: () => import('./pages/menu/menu.page').then( m => m.MenuPage)
   },
+  {
+    path: 'capturar',
+    loadComponent: () => import('./pages/capturar/capturar.page').then( m => m.CapturarPage)
+  },
 ];
